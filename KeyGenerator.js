5db511010017adc10139927bc782699efb6555ea
Phillip Lee <20524533+ph0ph0@users.noreply.github.com>
2021-09-16 15:50:06 +0100

    updated it so that the keys are saved as base64

--- KeyGenerator.js
+++ KeyGenerator.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require("fs");
 const { generateKeyPair, publicEncrypt, privateDecrypt } = require("crypto");
 //generate the key pair
 generateKeyPair(
@@ -22,38 +22,45 @@ generateKeyPair(
     console.log({
       publicKey,
       privateKey,
-    });//Print the keys to the console or save them to a file.
+    }); //Print the keys to the console or save them to a file.
 
-    fs.writeFileSync("publicKey.pem", publicKey);
-    fs.writeFileSync("privateKey.pem", privateKey);
+    fs.writeFileSync(
+      "publicKey.pem",
+      Buffer.from(publicKey).toString("base64")
+    );
+    fs.writeFileSync(
+      "privateKey.pem",
+      Buffer.from(privateKey).toString("base64")
+    );
     /*
-    * At this point you will have two pem files, 
-    * the public key which will start with 
-    * '-----BEGIN RSA PUBLIC KEY-----\n' +
-    * and the private key which will start with
-    * '-----BEGIN RSA PRIVATE KEY-----\n' +
-    */
+     * At this point you will have two pem files,
+     * the public key which will start with
+     * '-----BEGIN RSA PUBLIC KEY-----\n' +
+     * and the private key which will start with
+     * '-----BEGIN RSA PRIVATE KEY-----\n' +
+     */
     //Verify it works by encrypting and decrypting data
 
-    const data = "My secret data"
-    
+    const data = "My secret data";
+
     const encryptedData = publicEncrypt(
-        {
-            key: publicKey,
-            oaepHash: "sha256"
-        }, Buffer.from(data)
-        )
+      {
+        key: publicKey,
+        oaepHash: "sha256",
+      },
+      Buffer.from(data)
+    );
     // The encrypted data is in the form of bytes, so we print it in base64 format
-// so that it's displayed in a more readable form
+    // so that it's displayed in a more readable form
     console.log("encypted data: ", encryptedData.toString("base64"));
 
     const decryptedData = privateDecrypt(
-        {
-            key: privateKey,
-            oaepHash: "sha256"
-        },
-        encryptedData
-    )
+      {
+        key: privateKey,
+        oaepHash: "sha256",
+      },
+      encryptedData
+    );
 
     // The decrypted data is of the Buffer type, which we can convert to a
     // string to reveal the original data
